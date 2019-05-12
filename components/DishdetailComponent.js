22eedf6640f675a94a7c4c4455f1894e93f5f4ce
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-05-13 04:37:57 +0700

    Gestures Part 2

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -28,6 +28,8 @@ const mapDispatchToProps = dispatch => ({
 function RenderDish(props) {
   const dish = props.dish;
 
+  handleViewRef = ref => (this.view = ref);
+
   const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
     if (dx < -200) return true;
     else return false;
@@ -36,6 +38,13 @@ function RenderDish(props) {
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: (e, gestureState) => {
       return true;
+    },
+    onPanResponderGrant: () => {
+      this.view
+        .rubberBand(1000)
+        .then(endState =>
+          console.log(endState.finished ? "finished" : "cancelled")
+        );
     },
     onPanResponderEnd: (e, gestureState) => {
       if (recognizeDrag(gestureState))
@@ -66,6 +75,7 @@ function RenderDish(props) {
   if (dish != null) {
     return (
       <Animatable.View
+        ref={this.handleViewRef}
         {...panResponder.panHandlers}
         animation="fadeInDown"
         duration={1000}
